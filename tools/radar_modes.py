9cb963ac740c2ad43d5361384a4c4787300b03d8
Andrew Kiene <andrewkiene@dyn-137-229-27-131.net.gi.alaska.edu>
2016-10-27 14:56:56 -0800

    Fixed args for November Kodiak schedule

--- tools/radar_modes.py
+++ tools/radar_modes.py
@@ -179,7 +179,7 @@ radars['kod.c']["modes"]['Special:ST-APOG']={"controlprogram":"normalscan","args
 radars['kod.c']["modes"]['Special:ST-APOG']["altprogram"]="normalscan"
 radars['kod.c']["modes"]['Special:ST-APOG']["altargs"]="-sb 3 -eb 3 -fast -df 10400 -nf 10400"
 
-radars['kod.c']["modes"]['Special:World Day']={"controlprogram":"iwdscan","args":"-sb 3 -eb 3 -fast -df 10400 -nf 10400","priority":"0", "duration":"a"}
+radars['kod.c']["modes"]['Special:World Day']={"controlprogram":"iwdscan","args":"-startbeam 0 -stopbeam 8 -df 10400 -nf 10400","priority":"0", "duration":"a"}
 # Alternative program if radar not participating in Special program
 radars['kod.c']["modes"]['Special:World Day']["altprogram"]="normalscan"
 radars['kod.c']["modes"]['Special:World Day']["altargs"]="-sb 3 -eb 3 -fast -df 10400 -nf 10400"
@@ -199,8 +199,7 @@ radars['kod.d']["modes"]['Special:ST-APOG']={"controlprogram":"rbspscan","args":
 # Alternative program if radar not participating in Special program 
 radars['kod.d']["modes"]['Special:ST-APOG']["altprogram"]="normalsound"
 radars['kod.d']["modes"]['Special:ST-APOG']["altargs"]="-fast -df 10400 -nf 10400"
-radars['kod.d']["modes"]['Special:World Day']={"controlprogram":"iwdscan","args":"-sb 3 -eb 3 -fast -df 10400 -nf 10400","priority":"0", "duration":"a"}
-#radars['kod.d']["modes"]['Special:ST-APOG']={"controlprogram":"normalscan","args": "-df 10750 -nf 10750 -fast","priority":"1","duration": "a"}
+radars['kod.d']["modes"]['Special:World Day']={"controlprogram":"iwdscan","args":"-startbeam 0 -stopbeam 8 -df 10400 -nf 10400","priority":"0", "duration":"a"}
 # Alternative program if radar not participating in Special program
 radars['kod.d']["modes"]['Special:World Day']["altprogram"]="normalsound"
 radars['kod.d']["modes"]['Special:World Day']["altargs"]="-fast -df 10400 -nf 10400"
